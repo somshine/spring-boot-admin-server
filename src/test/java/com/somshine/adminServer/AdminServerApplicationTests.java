84c20da863b7d65216cbcaa98f861a356dcea112
Somnath Shinde <soma.shinde@gmail.com>
2021-08-12 10:46:35 +0530

    Admin server to manage the client information - first repository commit

---
+++ src/test/java/com/somshine/adminServer/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somshine.adminServer;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
